25898dafa87523c65e4cdcc1ed681ba56831b221
Nahid Ahmed <npahmed92@gmail.com>
2024-03-28 15:35:50 -0400

    testing push

--- test/Drink.test.js
+++ test/Drink.test.js
@@ -6,11 +6,31 @@ describe('Drink', () => {
       const drink = new Drink();
       expect(drink).toBeInstanceOf(Drink);
     });
+    // should have a name
+    it('should be a name property', () => {
+      const drinkName = 'Green Tea Latte';
+      const drink = new Drink(drinkName);
+      expect(drink.name).toBe(drinkName);
+    });
+    // should have a milk which defaults to none
+    it('should be a milk which defaults to null', () => {
+      const drinkName = 'Green Tea Latte';
+      const drink = new Drink(drinkName);
+      expect(drink.milk).toBe(null);
+    });
+    // sweetner: sugar, sweetnlow, equal, splenda
+    it('should be a sweetener  which defaults to null', () => {
+      const drinkName = 'Green Tea Latte';
+      const drink = new Drink(drinkName);
+      expect(drink.sweetener).toBe(null);
+    });
+    // sweetner count: 0
+    it('should be a sweetener count  which defaults to 0', () => {
+      const drinkName = 'Green Tea Latte';
+      const drink = new Drink(drinkName);
+      expect(drink.sweetenerCount).toBe(0);
+    });
+    // size: small, medium, large
+    // shots: 0
   });
 });
-// should have a name
-// should have a milk which defaults to none
-// sweetner: sugar, sweetnlow, equal, splenda
-// sweetner count: 0
-// size: small, medium, large
-// shots: 0
